ec697c6621d8308e297f2163da1cd67a42c02354
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-11-29 13:05:55 -0500

    Design: Navbar

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,12 @@ import Escena4 from './components/Escena4'
 import Escena3 from './components/Escena3'
 import Escena5 from './components/Escena5'
 import Navbar from './components/Navbar'
+import Escena2 from './components/Escena2'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-<<<<<<< HEAD
     <Navbar/>
-=======
     <Escena2/>
->>>>>>> 4239d2ce9c9a2a6fd4999b82cf2448b5ac671377
     <Escena3/>
     <Escena4/>
     <Escena5/>
