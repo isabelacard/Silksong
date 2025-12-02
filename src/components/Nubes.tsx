31372b243143954e7b0c76b4165d815d06c21fe0
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-12-02 04:06:24 -0500

    final deploy

--- src/components/Nubes.tsx
+++ src/components/Nubes.tsx
@@ -1,9 +1,11 @@
+import nubes from "../../public/banner/holip.png";
+
 export default function Nubes() {
     return (
         <div className="pointer-events-none">
             <img
-                src="/banner/holip.png"
-                className="absolute top-151 left-0 w-full h-auto z-[12] opacity-80"
+                src={nubes}
+                className="absolute lg:top-[610px] 2xl:top-[490px] left-0 w-full h-auto z-[12] opacity-80"
                 alt="nubes"
             />
         </div>
